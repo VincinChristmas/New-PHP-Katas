6ac9c51258a991f6b47bcf24984b2d2d5eeb28a9
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-04 21:06:19 -0600

    added a new php kata today

--- index.php
+++ index.php
@@ -6,9 +6,33 @@ This code does not execute properly. Try to figure out why.
 */
 
 //MY SOLUTION
-
 function multiply($a, $b) {
     return $a * $b;
+}
+
+//NUMBER 2
+
+/*
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+*/
+
+//MY SOLUTION
+function periodIsLate($last, $today, $cycleLength) {
+    
+    $interval = $last->diff($today);
+    
+    
+    return $interval->days > $cycleLength;
 }
 
 
