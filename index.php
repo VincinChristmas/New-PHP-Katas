e2b3ad9438302c00cc187159cf47a1392dc805c4
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-08 15:55:56 -0600

    added a php solution today

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php 
-//NUMBER !
+//NUMBER 3
+
+/*
+Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. 
+If a player loses a round, it's 
+the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
+*/
+
+//MY SOLUTION
+
+function whose_move(string $last_player, bool $win): string {
+  
+    if ($last_player === "black" && $win === false) {
+      return "white";
+    }
+    else if ($last_player === "white" && $win === true) {
+      return "white";
+    }
+     else if ($last_player === "white" && $win === false) {
+      return "black";
+    }
+    else if ($last_player === "black" && $win === true) {
+      return "black";
+    }
+  }
+//NUMBER 2
 
 /*
 This code does not execute properly. Try to figure out why.
@@ -10,7 +35,7 @@ function multiply($a, $b) {
     return $a * $b;
 }
 
-//NUMBER 2
+//NUMBER 1
 
 /*
 In this kata, we will make a function to test whether a period is late.
