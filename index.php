0a1e194153a0b8cda91794b44c67a8f74fb67eb3
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-29 22:09:50 -0600

    added a new php kata today

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
+//NUMBER 13
+
+/*    
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented 
+by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points 
+our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+//MY SOLUTION
+
 <?php 
+function points(array $games): int {
+  $sumX = 0;
+  foreach ($games as $game) {
+    if (intval($game[0]) > intval($game[2])) {
+      $sumX += 3;
+      
+    }
+    if (intval($game[0]) < intval($game[2])) {
+      $sumX += 0;
+      
+    }
+    if (intval($game[0]) == intval($game[2])) {
+      $sumX += 1;
+     
+    }
+  }
+  return $sumX;
+}
 //NUMBER 12 
 
 /*
