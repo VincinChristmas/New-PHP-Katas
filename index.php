b63600f4ef51f17d35435176c0fef85f5b943e83
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-30 18:55:45 -0600

    added a new php kata today

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php 
+//NUMBER 14
+/*
+There are pillars near the road. The distance between the pillars is the same and the width of 
+the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+*/
+
+//MY SOLUTION
+function pillars($numberOfPillars, $dist, $width)
+{
+  if ($numberOfPillars === 1) {
+    return 0;
+  }
+  elseif ($numberOfPillars === 2) {
+    return $dist * 100;
+  }
+  else {
+  return (($numberOfPillars - 1 ) * ($dist * 100)) + ($numberOfPillars - 2) * $width;
+}
+  }
+
 //NUMBER 13
 
 /*    
@@ -24,8 +50,6 @@ our team always plays 10 matches in the championship
 */
 
 //MY SOLUTION
-
-<?php 
 function points(array $games): int {
   $sumX = 0;
   foreach ($games as $game) {
