1cf4e905540abd1c09611f4ef7119dca78e61de0
VincinChristmas <VincinChristmas@hotmail.com>
2025-02-02 16:11:22 -0600

    added a new kata today

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php 
+//NUMBER 16
+
+/*
+If you can't sleep, just count sheeps!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a 
+murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+//MY SOLUTION
+function countsheep($num){
+  $sum = 0;
+  $allSheep = [];
+  if ($num === 0) {
+    return '';
+  }
+  else {
+  while ($sum < $num) {
+     $sum++;
+    array_push($allSheep, $sum . " " . 'sheep...');
+
+    }
+  }  
+  $countedSheep = join($allSheep);
+  return $countedSheep;
+}
 //NUMBER 15
 
 /*
