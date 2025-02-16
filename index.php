b4b3b2a430e5cb3862acf082407462b88ed61356
VincinChristmas <VincinChristmas@hotmail.com>
2025-02-15 18:56:32 -0600

    added a new kata today

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php 
 
+//NUMBER 23
+
+/* 
+Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+Some cases:
+[22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+*/
+
+//MY SOLUTION
+function multipleOfIndex(array $arr) : array {
+  $correctArr = [];
+  for ($i = 0; $i < count($arr); $i++) {
+   if ($i === 0) {
+            if ($arr[$i] === 0) {
+                array_push($correctArr, $arr[$i]);
+            }
+            continue;
+        }
+    if (($arr[$i] % $i) === 0) {
+      
+      array_push($correctArr, $arr[$i]);
+    }
+  }
+  return $correctArr;
+}
+
 //NUMBER 22
 
 /*
