96ca17408ce6cbb2cf1ba23392eb45674ccea0a3
VincinChristmas <VincinChristmas@hotmail.com>
2025-02-23 17:50:51 -0600

    added a new php kata today

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php 
 
+//NUMBER 27
+
+/*
+A hero is on his way to the castle to complete his mission. 
+However, he's been told that the castle is surrounded with a couple of powerful dragons! 
+each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. 
+Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+*/
+
+//MY SOLUTION
+function hero(int $bullets, int $dragons): bool{
+  if ($dragons === 0) {
+    return true;
+  }
+  else if (($bullets / $dragons) >= 2) {
+    return true;
+  }
+else {
+  return false;
+}
+}
+
 //NUMBER 26
 
 /*
