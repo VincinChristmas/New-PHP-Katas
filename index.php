5c19125ee0bc7fa8b760d96cef6874dbd93e8980
VincinChristmas <VincinChristmas@hotmail.com>
2025-02-22 20:33:34 -0600

    added a new php kata today

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php 
 
+//NUMBER 26
+
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+//MY SOLUTION
+function reverseSeq (int $n): array {
+  $splinter = [];
+    while ($n > 0) {
+      array_push($splinter, $n);
+      $n = $n - 1;
+    }
+  return $splinter;
+
 //NUMBER 25
 
 /*
