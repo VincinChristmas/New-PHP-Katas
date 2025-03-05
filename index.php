01dfeba0b3fa8177d503626f354e61eccc6765be
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-05 16:39:06 -0600

    completed a new php kata today

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php 
+//NUMBER 31
+
+/*
+In this simple exercise, you will write a function that takes two integers; n and limit; and
+ returns a list of the multiples of n up to and possibly including limit.
+
+It is guaranteed that n > 0 and limit >= n.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 
+are the multiples of 2 up to 6.
+
+Examples
+n = 2; limit = 6 --> [2, 4, 6]
+n = 2; limit = 5 --> [2, 4]
+*/
+
+//MY SOLUTION
+function findMultiples(int $integer, int $limit): array{
+  $sum = 0;
+  $finalArray = [];
+  $multiple = $integer;
+  while ($multiple <= $limit) {
+    array_push($finalArray, $multiple);
+    $multiple += $integer;  
+  }
+  return $finalArray;
+}
+
 //NUMBER 30
 
 /*
